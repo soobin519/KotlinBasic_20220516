2c8fb33ee264231acf41e3d4fa1aa23641c7b2b3
soobin519 <soobin0519@naver.com>
2022-05-16 14:24:59 +0900

    logclick 이벤트 처리 - return true 필요

--- app/src/main/java/kr/co/asianaidt/kotlinbasic_20220516/MainActivity.kt
+++ app/src/main/java/kr/co/asianaidt/kotlinbasic_20220516/MainActivity.kt
@@ -2,10 +2,24 @@ package kr.co.asianaidt.kotlinbasic_20220516
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener {
+
+            //btnlog가 클릭되면 할일
+            Log.d("메인화면", "로그 찍기 버튼 눌림")
+        }
+
+        btnLog.setOnLongClickListener {
+            Log.d("메인화면", "로그 찍기 버튼 길게 눌림")
+
+            return@setOnLongClickListener true
+        }
     }
 }
